c08230602393f898a522ed9d22cc9ab46bf29bd6
keram86 <126859648+keram86@users.noreply.github.com>
2023-03-03 13:24:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('test title')
 streamlit.text('test text')
 streamlit.header('test text 2')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
